74c24871f55b4bcdd434b29b0aacbc92b43b76a6
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-02 19:18:32 +0530

    first time run the c and cpp file in the vs code

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ how to run c program in visual studio code 
+ command to run c program in visual studio code
+ gcc hello.c -o hello
+ ./hello.exe
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "yash";
+
+    return 0;
+}
