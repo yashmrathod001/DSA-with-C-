fc80213382eafe45843178796a5c2cdd0d677198
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-04 17:28:36 +0530

    third pattur

---
+++ auto functions 50Q/star patturns/second.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter a int :" << endl;
+    cin >> n;
+
+    for(int i=0; i < n ; i++)
+    {
+        for(int j =0 ; j <= i ; j++)
+        {
+            cout<< "* ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
---
+++ auto functions 50Q/star patturns/try_3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter a int :" << endl;
+    cin >> n;
+
+    for(int i=0; i < n ; i++)
+    {
+        for(int j = i ; j < n; j++)
+        {
+            cout<< "  ";
+        }
+
+        for(int j =0 ; j <= i ; j++)
+        {
+            cout<< "* ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
