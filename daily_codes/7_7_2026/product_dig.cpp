6cffae757f878ec1e8c5a46c992bec50a57254ad
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-07 18:18:43 +0530

    product of digit

---
+++ daily_codes/7_7_2026/product_dig.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter a number :";
+	cin>> n;
+	int sum = 1;
+	while(n > 0)
+	{
+		int digit = n % 10;
+		sum = sum * digit;
+		n /= 10;	
+	}
+	cout<< sum ;
+	
+	return 0;
+}
