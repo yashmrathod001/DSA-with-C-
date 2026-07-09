b2096ccc3212fe5131a6918cf88f2edfe2c8c893
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-09 18:04:42 +0530

    check odd even

---
+++ daily_codes/9_7_2026/statements/_2_check_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter a number :";
+	cin>> n ;
+	if(n%2==0)
+	{
+		cout<<"the given number is even .";
+	}
+	else{
+		cout<<" the given number is odd.";
+	}
+	return 0;
+}
