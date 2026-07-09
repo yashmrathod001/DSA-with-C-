e99819db380b9cb6d41cadd0c43bac8f6e3ee882
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-09 17:47:24 +0530

    make a new folder and save the files in the proper structure

---
+++ daily_codes/9_7_2026/basics and arithmetic/_9relational operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// relational operators 
+	int n1 = 10, n2 = 12 ;
+	cout<<(n1 < n2);
+	cout<<" true = 1 and false = 0 ";
+	cout <<"\n greater : "<<(n1 > n2);
+	cout <<"\n less : "<<(n1 < n2);
+	cout <<"\n greaterthen and equal : "<<(n1>= n2);
+	cout <<"\n less then and equal : "<<(n1 <= n2);
+	cout <<"\n is equal to : "<<(n1 == n2);
+	cout <<"\n not equal to : "<<(n1 != n2);
+	
+	return 0;
+}
