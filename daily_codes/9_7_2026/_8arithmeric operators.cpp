6e3d54d924411c44e550214193c490be6730b98d
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-09 17:36:17 +0530

    all arithmetic operators

---
+++ daily_codes/9_7_2026/_8arithmeric operators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// arithmetic operators 
+	int n1 = 21 , n2 = 2;
+	int sum = n1 + n2 ;
+	int sub = n1 - n2 ;
+	int mul = n1 * n2 ;
+	float div = n1 / n2 ;
+	int mode = n1 % n2 ;
+	cout<<" sum = " << sum ;
+	cout<<"\n sub =" << sub ; 
+	cout<<"\n mul =" << mul ;
+	cout<<"\n div =" << div ;
+	cout<<"\n moduluse = " << mode ;
+	return 0;
+}
