d904a4abad24662cb1129276270ff148d92eed30
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-11 12:29:47 +0530

    taking input of array

---
+++ daily_codes/6_Array/_2_input_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int arr[5] ;
+	cout<<"enter 5 element of the array";
+	for(int i=0;i<5;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	
+	for(int i=0;i<5;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+
+}
